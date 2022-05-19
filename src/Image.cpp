5bcaa1b07875d0cca689314b649a82cd6cc87497
Fgdou <fabigoardou@gmail.com>
2022-05-19 17:50:24 +0200

    Add Chest

--- src/Image.cpp
+++ src/Image.cpp
@@ -2,13 +2,17 @@
 // Created by fgdou on 5/19/22.
 //
 
+#include <stdexcept>
 #include "Image.h"
 
-Image::Image(const std::string &path) : _texture(nullptr)
+Image::Image(const std::string &path) : _texture(nullptr), path(path)
 {
   SDL_Renderer &sdl_renderer = Renderer::getInstance().getSLDRenderer();
 
   _texture = IMG_LoadTexture(&sdl_renderer, path.c_str());
+
+  if(_texture == nullptr)
+      throw std::runtime_error("Failed to open image");
 }
 
 Image::~Image()
@@ -19,4 +23,23 @@ Image::~Image()
 SDL_Texture *Image::get() const
 {
   return _texture;
+}
+
+Image::Image(const Image &copy)
+    : _texture(nullptr), path(copy.path)
+{
+    SDL_Renderer &sdl_renderer = Renderer::getInstance().getSLDRenderer();
+    _texture = IMG_LoadTexture(&sdl_renderer, path.c_str());
+}
+
+Image &Image::operator=(const Image &copy) {
+    SDL_DestroyTexture(_texture);
+
+    _texture = nullptr;
+    path = copy.path;
+
+    SDL_Renderer &sdl_renderer = Renderer::getInstance().getSLDRenderer();
+    _texture = IMG_LoadTexture(&sdl_renderer, path.c_str());
+
+    return *this;
 }
