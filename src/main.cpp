50fab0bfbc266e16f1a10ed0dc90fc9730dd976e
Fgdou <fabigoardou@gmail.com>
2022-05-19 08:18:31 +0200

    Init project

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by fgdou on 5/19/22.
+//
+
+#include "main.h"
+
+int main(int argc, char** argv){
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,14 @@
+//
+// Created by fgdou on 5/19/22.
+//
+
+#ifndef PROJPROG2022_MAIN_H
+#define PROJPROG2022_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PROJPROG2022_MAIN_H
