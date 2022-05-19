2ab0e1a541fc30129736e592607c974c866ed4f1
CodyAdam <cody.ad4@gmail.com>
2022-05-19 19:32:35 +0200

    Fix colliision

--- src/Rooms/Room.cpp
+++ src/Rooms/Room.cpp
@@ -5,8 +5,9 @@
 #include "Room.h"
 #include "../Entities/DynamicEntity.h"
 #include "../World.h"
+#include <algorithm>
 
-Room::Room(std::vector<std::shared_ptr<DynamicEntity>> entities, std::vector<std::shared_ptr<Wall>> walls, World & world) : _entities(entities), _walls(walls), _isCleared(false), _world(world)
+Room::Room(std::vector<std::shared_ptr<DynamicEntity>> entities, std::vector<std::shared_ptr<Wall>> walls, World &world) : _entities(entities), _walls(walls), _isCleared(false), _world(world)
 {
 }
 
@@ -47,13 +48,14 @@ std::shared_ptr<Player> Room::getPlayer() const
     return _world.getPlayer();
 }
 
-World & Room::getWorld() const
+World &Room::getWorld() const
 {
     return _world;
 }
 
 Collision Room::getPlayerCollision(Vec2 pos, Vec2 mov)
 {
+    Collision collision = {false, {}, {}};
     for (std::shared_ptr<Wall> w : _walls)
     {
         Collision c = Collision::getLineRectCollision(
@@ -63,22 +65,34 @@ Collision Room::getPlayerCollision(Vec2 pos, Vec2 mov)
             w->getPos() + w->getSize());
         if (c.isCollide())
         {
-            return Collision(c.isCollide(), c.getImpact(), mov * c.getRebound());
+            if (!collision.isCollide() || pos.distance(c.getImpact()) < pos.distance(collision.getImpact()))
+                collision = Collision(c.isCollide(), c.getImpact(), mov * c.getRebound());
         }
-
-        }
-    auto screenSize = Renderer::getInstance().getSize();
-    Vec2 ScreenPos = (pos+mov).worldToScreen();
-    Vec2 currentRoom = _world.getSelectedRoom();
-    if(ScreenPos.x > screenSize.x){
-        _world.setSelectedRoom((currentRoom.x+1,currentRoom.y));
-    } else if( ScreenPos.x <0){
-        _world.setSelectedRoom((currentRoom.x-1,currentRoom.y));
-    } else if( ScreenPos.y > screenSize.y){
-        _world.setSelectedRoom((currentRoom.x,currentRoom.y+1));
-    } else if( ScreenPos.y < 0){
-        _world.setSelectedRoom((currentRoom.x,currentRoom.y-1));
     }
+    if (collision.isCollide())
+        return collision;
+
+    // check changement de room
+    auto screenSize = Renderer::getInstance().getSize();
+    Vec2 ScreenPos = (pos + mov).worldToScreen();
+    Vec2 currentRoom = _world.getSelectedRoom();
+    if (ScreenPos.x > screenSize.x)
+    {
+        _world.setSelectedRoom((currentRoom.x + 1, currentRoom.y));
+    }
+    else if (ScreenPos.x < 0)
+    {
+        _world.setSelectedRoom((currentRoom.x - 1, currentRoom.y));
+    }
+    else if (ScreenPos.y > screenSize.y)
+    {
+        _world.setSelectedRoom((currentRoom.x, currentRoom.y + 1));
+    }
+    else if (ScreenPos.y < 0)
+    {
+        _world.setSelectedRoom((currentRoom.x, currentRoom.y - 1));
+    }
+
     return Collision(false, {}, {});
 }
 
