837df1f2563ad6e34a59615fcfb0a9f1f4616105
fgdou <fabigoardou@gmail.com>
2022-05-19 21:39:23 +0200

    Fix collision Update chest to handle opening (not fully working)

--- src/Entities/Chest.cpp
+++ src/Entities/Chest.cpp
@@ -24,6 +24,12 @@ void Chest::draw() {
 }
 
 void Chest::update(Room &room) {
-    //TODO interact with player
-//    Collision::getPointInsideRectCollision(room)
+    if(item == nullptr)
+        return;
+
+    auto col = Collision::getPointInsideRectCollision(room.getPlayer()->getPos() ,getPos()+size, getPos()-size);
+    if(col.isCollide()){
+        room.getPlayer()->lootEquippableItem(item);
+        item = nullptr;
+    }
 }
