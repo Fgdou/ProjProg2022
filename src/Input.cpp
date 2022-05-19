d53d6aed9924dc82562e789a27a8f80d7c710012
Fgdou <fabigoardou@gmail.com>
2022-05-19 10:13:07 +0200

    Finish Input

--- src/Input.cpp
+++ src/Input.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "Input.h"
+
+std::unique_ptr<Input> ptr;
+
+Input &Input::getInstance() {
+    if(ptr == nullptr)
+        ptr = std::make_unique<Input>();
+    return *ptr;
+}
+
+bool Input::isPressed(Input::Inputs input) {
+    return inputs[input];
+}
+
+bool Input::hasBeenPressedOnce(Input::Inputs input) {
+    if(pressedOnce[input]){
+        pressedOnce[input] = false;
+        return true;
+    }
+    return false;
+}
+
+const Vec2 &Input::getMousePos() {
+    return mousePos;
+}
+
+void Input::update(SDL_Event &e) {
+
+    if(e.type == SDL_MOUSEMOTION){
+        mousePos = Vec2(
+                e.motion.x,
+                e.motion.y
+        ).screenToWorld();
+    }else if(e.type == SDL_MOUSEBUTTONDOWN){
+        if(e.button.button == SDL_BUTTON_LEFT)
+            setButtonTrue(MouseLeft);
+        else if(e.button.button == SDL_BUTTON_RIGHT)
+            setButtonTrue(MouseRight);
+        else if(e.button.button == SDL_BUTTON_MIDDLE)
+            setButtonTrue(MouseMiddle);
+    }else if(e.type == SDL_MOUSEBUTTONUP){
+        if(e.button.button == SDL_BUTTON_LEFT)
+            inputs[MouseLeft] = false;
+        else if(e.button.button == SDL_BUTTON_RIGHT)
+            inputs[MouseRight] = false;
+        else if(e.button.button == SDL_BUTTON_MIDDLE)
+            inputs[MouseMiddle] = false;
+    }else if(e.type == SDL_KEYDOWN){
+        if(e.key.keysym.sym == SDLK_ESCAPE)
+            setButtonTrue(escape);
+    }else if(e.type == SDL_KEYUP){
+        if(e.key.keysym.sym == SDLK_ESCAPE)
+            inputs[escape] = false;
+    }
+}
+
+void Input::setButtonTrue(Input::Inputs i) {
+    inputs[i] = true;
+    pressedOnce[i] = true;
+}
--- src/Input.h
+++ src/Input.h
@@ -7,27 +7,31 @@
 
 
 #include "Maths/Vec2.h"
+#include <SDL2/SDL.h>
+#include <map>
 
 class Input {
 public:
-    enum class Inputs{
+    enum Inputs{
         MouseLeft,
         MouseRight,
         MouseMiddle,
-        up,
-        down,
-        left,
-        right,
-        jump,
         escape,
     };
 
-    void update();
+    void update(SDL_Event& e);
     const Vec2& getMousePos();
     bool isPressed(Inputs input);
     bool hasBeenPressedOnce(Inputs input);
 
     static Input& getInstance();
+
+private:
+    void setButtonTrue(Inputs i);
+
+    std::map<Inputs, bool> inputs;
+    std::map<Inputs, bool> pressedOnce;
+    Vec2 mousePos;
 };
 
 
