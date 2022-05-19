16ba456cbd09b8bfcf56123fd4212dd55f083b1a
Gpinault <gpinault35@gmail.com>
2022-05-19 18:11:07 +0200

    Création BaseEnemy (incomplet)

---
+++ src/Entities/BaseEnemy.cpp
@@ -0,0 +1,22 @@
+//
+// Created by guill on 19/05/2022.
+//
+
+#include "BaseEnemy.h"
+#include "../Rooms/Room.h"
+
+BaseEnemy::BaseEnemy(Vec2 & pos, double lifemax, double damage) : DamageableEntity(pos, lifemax), damage(damage){}
+
+void BaseEnemy::update(Room & room) {
+        auto player = room.getPlayer();
+        Vec2 movement_vector = position.distance(player->getPos());
+        Vec2 vec_move = movement_vector*speed ;
+        auto c = room.getPlayerCollision(this->position, vec_move);
+        if(c.isCollide()){
+            this->setPos(c.getImpact());
+            this->movement_vector = c.getRebound().normalize();
+        } else {
+            this->setPos(this->getPos() + this->movement_vector*speed);
+            (this->movement_vector*speed).print();
+        }
+}
---
+++ src/Entities/BaseEnemy.h
@@ -0,0 +1,24 @@
+//
+// Created by guill on 19/05/2022.
+//
+
+#ifndef PROJPROG2022_ENEMY_H
+#define PROJPROG2022_ENEMY_H
+
+#include "DamageableEntity.h"
+#include "Player.h"
+
+class BaseEnemy : public DamageableEntity {
+protected:
+    double damage;
+    double speed;
+    Vec2 movement_vector;
+
+public:
+    BaseEnemy(Vec2 & pos, double lifemax, double damage);
+
+    void update(Room & room);
+};
+
+
+#endif //PROJPROG2022_ENEMY_H
