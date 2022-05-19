264803c8de11d29151c856e16cc543f995781aa6
CodyAdam <cody.ad4@gmail.com>
2022-05-19 09:21:00 +0200

    Fix getInstance

--- src/Renderer.h
+++ src/Renderer.h
@@ -5,27 +5,26 @@
 #ifndef PROJPROG2022_RENDERER_H
 #define PROJPROG2022_RENDERER_H
 
-
 #include <string>
 #include "Maths/Vec2.h"
 
 #include "SDL2/SDL.h"
 
-class Renderer {
+class Renderer
+{
 public:
-//    static Renderer& getInstance(){
-//        return Renderer();
-//    }
+    static Renderer &getInstance()
+    {
+        return Renderer();
+    }
 
-    void drawCircle(const Vec2& pos, double size, const SDL_Color& c);
-    void drawRect(const Vec2& center, const Vec2& halfSize, const SDL_Color& c);
-    void drawImage(const std::string& path, const Vec2& center, const Vec2& halfSize);
-    void drawText(const std::string& text, const Vec2& p1, double size);
+    void drawCircle(const Vec2 &pos, double size, const SDL_Color &c);
+    void drawRect(const Vec2 &center, const Vec2 &halfSize, const SDL_Color &c);
+    void drawImage(const std::string &path, const Vec2 &center, const Vec2 &halfSize);
+    void drawText(const std::string &text, const Vec2 &p1, double size);
     void clear();
 
-    const Vec2& getSize();
-
+    const Vec2 &getSize();
 };
 
-
-#endif //PROJPROG2022_RENDERER_H
+#endif // PROJPROG2022_RENDERER_H
