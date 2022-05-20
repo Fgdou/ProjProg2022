a4b5d2b96c1c2165b92f85a38be3f173b1d9a1c8
Gpinault <gpinault35@gmail.com>
2022-05-20 11:40:17 +0200

    Interaction Boss/Player

--- src/Entities/Enemies/Boss.cpp
+++ src/Entities/Enemies/Boss.cpp
@@ -3,7 +3,43 @@
 //
 
 #include "Boss.h"
+#include "../../Maths/Timer.h"
 
-Boss::Boss(Vec2 & pos, double lifemax, double damage): BaseEnemy(pos, lifemax, damage){
+Boss::Boss(Vec2 & pos, double lifemax, double damage): BaseEnemy(pos, lifemax, damage), distance(0.0), distanceMax(0.4){}
 
+double Boss::getDistance() {
+    return distance;
+}
+
+void Boss::update(Room & room){
+    if(this->cooldownDamage > 0.0){
+        this->cooldownDamage -= Timer::getDeltaTime();
+    } else if(distance < distanceMax){
+        distance += 0.05;
+    } else{
+        distance = 0;
+        this->hasAttackedPlayer();
+    }
+
+    if(this->timerHitDamage > 0.0){
+        this->timerHitDamage -= Timer::getDeltaTime();
+        Renderer::getInstance().drawCircle(this->getPos(), 20.0, {255, 0, 0, 255});
+        Renderer::getInstance().drawText(std::to_string(this->lastDamage), this->getPos()+Vec2(0.0, -30-70.0*this->timerHitDamage*2), 30, {255, 0, 0, 255});
+    } else {
+        Renderer::getInstance().drawCircle(this->getPos(), 20.0, {0, 255, 0, 255});
+    }
+}
+
+void Boss::draw() {
+    if(this->timerHitDamage > 0.0){
+        Renderer::getInstance().drawCircle(this->getPos(), 20.0, {255, 0, 0, 255});
+        Renderer::getInstance().drawText(std::to_string(this->lastDamage), this->getPos()+Vec2(0.0, -30-70.0*this->timerHitDamage*2), 30, {255, 0, 0, 255});
+    } else {
+        Renderer::getInstance().drawCircle(this->getPos(), 20.0, {0, 255, 0, 255});
+    }
+
+}
+
+void Boss::hasAttackedPlayer() {
+    this->cooldownDamage = 1.5;
 }
